cafc89579867f9f58d918d86c691b3dda0d8f813
Виталий Петрович <e2n0z1o5@gmail.com>
2018-10-18 23:21:08 +0300

    Add project files.

---
+++ NET1.S.2018.Petrovich.04.Test/SimpleTasksTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using static NET1.S._2018.Petrovich._04.SimpleTasks;
+
+namespace NET1.S._2018.Petrovich._04.Test
+{
+    [TestFixture]
+    public class SimpleTasksTest
+    {
+        [TestCase(new double[] { -29.0043d, 0.234d, -54.00001d },
+            ExpectedResult = new string[] { "minus two nine point zero zero four three", "zero point two three four", "minus five four point zero zero zero zero one" })]
+        [TestCase(new double[] { 845.33d, 43.09d },
+            ExpectedResult = new string[] { "eight four five point three three", "four three point zero nine" })]
+        [TestCase(new double[] { -1.279d, 76542.73d, -3.31d },
+            ExpectedResult = new string[] { "minus one point two seven nine", "seven six five four two point seven three", "minus three point three one" })]
+        public string[] TransformToWords_RightIput_RightOut(double[] realNumbers)
+            => TransformToWords(realNumbers);
+
+        [Test]
+        public void TransformToWords_CallWithNullReference_ThrowArgumentNullException()
+            => Assert.Throws<ArgumentNullException>(() => TransformToWords(null));
+
+        [Test]
+        public void TransformToWords_CallWithEmptyArray_ThrowArgumentException()
+            => Assert.Throws<ArgumentException>(() => TransformToWords(new double[0]));
+    }
+}
---
+++ NET1.S.2018.Petrovich.04/SimpleTasks.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET1.S._2018.Petrovich._04
+{
+    /// <summary>
+    /// Contain TransformToWords method.
+    /// </summary>
+    public static class SimpleTasks
+    {
+        /// <summary>
+        /// Transform array of doubles into array of strings.
+        /// Each digit and symbol represent by text equivalent.
+        /// </summary>
+        /// <param name="realNumbers">
+        /// Array of doubles for transform.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// Throws, when method is call with null array reference.
+        /// </exception>>
+        /// <exception cref="ArgumentException">
+        /// Throws, when method is call with array containing no elements.
+        /// </exception>>
+        /// <returns>
+        /// Resulted array fo strings.
+        /// </returns>
+        public static string[] TransformToWords(double[] realNumbers)
+        {
+            if (realNumbers == null)
+            {
+                throw new ArgumentNullException(nameof(realNumbers));
+            }
+
+            if (realNumbers.Length == 0)
+            {
+                throw new ArgumentException($"Array {nameof(realNumbers)} does not contain elements.");
+            }
+
+            string[] TransformToWordsAlgo(double[] arrayOfDouble)
+            {
+                string[] stringsArray = new string[arrayOfDouble.Length];
+
+                for (int i = 0; i < arrayOfDouble.Length; i++)
+                {
+                    stringsArray[i] = TransformToWord(arrayOfDouble[i]).TrimEnd();
+                }
+
+                return stringsArray;
+            }
+
+            return TransformToWordsAlgo(realNumbers);
+        }
+
+        private static string TransformToWord(double num)
+        {
+            StringBuilder accumulator = new StringBuilder();
+
+            foreach (var ch in num.ToString("R"))
+            {
+                switch (ch)
+                {
+                    case '-':
+                        accumulator.Append("minus ");
+                        break;
+                    case ',':
+                        accumulator.Append("point ");
+                        break;
+                    case '0':
+                        accumulator.Append("zero ");
+                        break;
+                    case '1':
+                        accumulator.Append("one ");
+                        break;
+                    case '2':
+                        accumulator.Append("two ");
+                        break;
+                    case '3':
+                        accumulator.Append("three ");
+                        break;
+                    case '4':
+                        accumulator.Append("four ");
+                        break;
+                    case '5':
+                        accumulator.Append("five ");
+                        break;
+                    case '6':
+                        accumulator.Append("six ");
+                        break;
+                    case '7':
+                        accumulator.Append("seven ");
+                        break;
+                    case '8':
+                        accumulator.Append("eight ");
+                        break;
+                    case '9':
+                        accumulator.Append("nine ");
+                        break;
+                    case 'E':
+                        accumulator.Append("exp ");
+                        break;
+
+                }
+            }
+
+            return new string(accumulator.ToString().ToCharArray());
+        }
+    }
+}
