c92436f0fbb1255bc9fd6c8613d4075573c79fa2
Виталий Петрович <e2n0z1o5@gmail.com>
2018-10-20 00:29:31 +0300

    Added 2nd Task.

--- NET1.S.2018.Petrovich.04/SimpleTasks.cs
+++ NET1.S.2018.Petrovich.04/SimpleTasks.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NET1.S._2018.Petrovich._04
@@ -102,11 +103,94 @@ namespace NET1.S._2018.Petrovich._04
                     case 'E':
                         accumulator.Append("exp ");
                         break;
-
                 }
             }
 
             return new string(accumulator.ToString().ToCharArray());
+        }
+
+    }
+    /// <summary>
+    /// Extension class for type double.
+    /// </summary>
+    public static class DoubleExtension
+    {
+        /// <summary>
+        /// Extension method for type double, which represent number in IEEE 754 format.
+        /// </summary>
+        /// <param name="inputDouble">
+        /// Number.
+        /// </param>
+        /// <returns>
+        /// String representation.
+        /// </returns>
+        public static string ToIEEE754(this double inputDouble)
+        {
+            switch (inputDouble)
+            {
+                case 0.0:
+                    if ((double.PositiveInfinity/inputDouble)*Double.PositiveInfinity == double.PositiveInfinity)
+                        return $"0000000000000000000000000000000000000000000000000000000000000000";
+                    else
+                        return $"1000000000000000000000000000000000000000000000000000000000000000";
+                case Double.NaN:
+                    return $"1111111111111000000000000000000000000000000000000000000000000000";
+                case Double.NegativeInfinity:
+                    return $"1111111111110000000000000000000000000000000000000000000000000000";
+                case Double.PositiveInfinity:
+                    return $"0111111111110000000000000000000000000000000000000000000000000000";
+            }
+
+            int exp = 0;
+            string acc = string.Empty;
+
+            if (inputDouble < 0.0)
+            {
+                acc += '1';
+                inputDouble *= -1.0;
+            }
+            else
+            {
+                acc += '0';
+            }
+
+            double mant = inputDouble;
+
+            while (mant > 2.0)
+            {
+                mant /= 2.0;
+                exp++;
+            }
+
+            while (mant < 1.0)
+            {
+                mant *= 2.0;
+                exp--;
+            }
+
+            acc += Convert.ToString(exp + 1023, 2);
+
+            if (mant == 1.0)
+            {
+                return acc.PadRight(64, '0');
+            }
+            else
+            {
+                mant -= 1.0;
+            }
+
+            int count = 0;
+            while (mant != 0.0 && count++ < 52)
+            {
+                mant *= 2.0;
+                acc += ((int)mant).ToString();
+                if (mant >= 1.0)
+                {
+                    mant -= 1.0;
+                }
+            }
+
+            return acc.PadRight(64, '0');
         }
     }
 }
